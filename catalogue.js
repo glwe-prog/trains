6282f62addba32383465c0d490ef134fa6855ddc
glwe-prog <glwe@hotmail.com>
2026-07-30 14:08:30 +0100

    Update class_802_transpennine: zoom 1.00 pos 50% 50%

--- catalogue.js
+++ catalogue.js
@@ -1307,5 +1307,36 @@ const CATALOGUE = [
     },
     "zoom": 1.0,
     "pos": "50% 50%"
+  },
+  {
+    "id": "class_802_transpennine",
+    "quizzes": {
+      "class": {
+        "name": "Class 802",
+        "blur": [
+          [
+            580,
+            379,
+            620,
+            406
+          ]
+        ],
+        "blurR": 12
+      },
+      "livery": {
+        "name": "TransPennine",
+        "blur": [
+          [
+            866,
+            306,
+            917,
+            343
+          ]
+        ],
+        "blurR": 10
+      }
+    },
+    "zoom": 1.0,
+    "pos": "50% 50%"
   }
 ];
