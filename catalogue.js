20b8e5137129d5f36d1739fce668726b10a12e6d
glwe-prog <glwe@hotmail.com>
2026-07-30 12:08:00 +0100

    Update class_390: zoom 1.00 pos 50% 50%

---
+++ catalogue.js
@@ -0,0 +1,981 @@
+const CATALOGUE = [
+  {
+    "id": "class_43_hst",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 43 HST (IC125)",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_220",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 220 Voyager",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_221",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 221 Super Voyager",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_222",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 222 Meridian",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_180",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 180 Adelante",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_91",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 91 (IC225)",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_390",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Avanti West Coast",
+        "blur": [
+          [
+            407,
+            304,
+            452,
+            342
+          ]
+        ],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "class_395",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 395 Javelin",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_800",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 800 Azuma",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_802",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 802",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_810",
+    "zoom": 1.6,
+    "pos": "59% 74%",
+    "quizzes": {
+      "class": {
+        "name": "Class 810 Aurora",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_153",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 153",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_155",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 155 Super Sprinter",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_156",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 156 Super Sprinter",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_158",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 158 Express Sprinter",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_159",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 159",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_170",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 170 Turbostar",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_171",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 171 Turbostar",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_175",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 175 Coradia",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_185",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 185 Desiro",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_195",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 195 Civity",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_197",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 197 Civity",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_313",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 313",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_315",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 315",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_317",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 317",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_319",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 319",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_321",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 321",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_323",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 323",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_331",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 331 Civity",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_333",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 333",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_350",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 350 Desiro",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_357",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 357",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_360",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 360 Desiro",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_365",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 365 Networker",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_375",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 375 Electrostar",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_377",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 377 Electrostar",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_380",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 380 Desiro",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_385",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 385 AT200",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_387",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 387 Electrostar",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_444",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 444 Desiro",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_450",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 450 Desiro",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_455",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 455",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_465",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 465 Networker",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_700",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 700 Desiro City",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_707",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 707 Desiro City",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_710",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 710 Aventra",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_720",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 720 Aventra",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_745",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 745",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_755",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 755 Flirt",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_37",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 37",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_47",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 47",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_57",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 57",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_66",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 66",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_67",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 67",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "class_68",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "class": {
+        "name": "Class 68",
+        "blur": [],
+        "blurR": 12
+      }
+    }
+  },
+  {
+    "id": "livery_avanti",
+    "zoom": 1.5,
+    "pos": "50% 70%",
+    "quizzes": {
+      "livery": {
+        "name": "Avanti West Coast",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_lner",
+    "zoom": 1.25,
+    "pos": "17% 85%",
+    "quizzes": {
+      "livery": {
+        "name": "LNER",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_gwr",
+    "zoom": 1.95,
+    "pos": "85% 86%",
+    "quizzes": {
+      "livery": {
+        "name": "Great Western Railway",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_emr",
+    "zoom": 1.6,
+    "pos": "59% 74%",
+    "quizzes": {
+      "livery": {
+        "name": "East Midlands Railway",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_crosscountry",
+    "zoom": 1.45,
+    "pos": "50% 22%",
+    "quizzes": {
+      "livery": {
+        "name": "CrossCountry",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_hull_trains",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Hull Trains",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_grand_central",
+    "zoom": 1.7,
+    "pos": "57% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Grand Central",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_heathrow_express",
+    "zoom": 2.2,
+    "pos": "50% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Heathrow Express",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_caledonian_sleeper",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Caledonian Sleeper",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_chiltern",
+    "zoom": 1.45,
+    "pos": "50% 96%",
+    "quizzes": {
+      "livery": {
+        "name": "Chiltern Railways",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_thameslink",
+    "zoom": 1.2,
+    "pos": "98% 4%",
+    "quizzes": {
+      "livery": {
+        "name": "Thameslink",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_southeastern",
+    "zoom": 1.4,
+    "pos": "19% 83%",
+    "quizzes": {
+      "livery": {
+        "name": "Southeastern",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_southern",
+    "zoom": 1.3,
+    "pos": "96% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Southern",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_greater_anglia",
+    "zoom": 1.45,
+    "pos": "50% 59%",
+    "quizzes": {
+      "livery": {
+        "name": "Greater Anglia",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_c2c",
+    "zoom": 1.35,
+    "pos": "53% 14%",
+    "quizzes": {
+      "livery": {
+        "name": "c2c",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_scotrail",
+    "zoom": 1.5,
+    "pos": "50% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "ScotRail",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_transpennine",
+    "zoom": 1.0,
+    "pos": "100% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "TransPennine Express",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_northern",
+    "zoom": 1.25,
+    "pos": "99% 29%",
+    "quizzes": {
+      "livery": {
+        "name": "Northern",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_merseyrail",
+    "zoom": 1.3,
+    "pos": "50% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Merseyrail",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_west_midlands",
+    "zoom": 1.4,
+    "pos": "2% 52%",
+    "quizzes": {
+      "livery": {
+        "name": "West Midlands Railway",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_tfw",
+    "zoom": 1.55,
+    "pos": "50% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Transport for Wales",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_db_cargo",
+    "zoom": 1.9,
+    "pos": "73% 60%",
+    "quizzes": {
+      "livery": {
+        "name": "DB Cargo UK",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_freightliner",
+    "zoom": 1.0,
+    "pos": "50% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Freightliner",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_gbrf",
+    "zoom": 1.35,
+    "pos": "28% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "GB Railfreight",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_colas",
+    "zoom": 1.6,
+    "pos": "50% 50%",
+    "quizzes": {
+      "livery": {
+        "name": "Colas Rail",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  },
+  {
+    "id": "livery_drs",
+    "zoom": 1.95,
+    "pos": "63% 71%",
+    "quizzes": {
+      "livery": {
+        "name": "Direct Rail Services",
+        "blur": [],
+        "blurR": 10
+      }
+    }
+  }
+];
