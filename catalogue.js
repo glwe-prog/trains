79dbf2b3f3a896de960f7dd4bce9bed621975117
glwe-prog <glwe@hotmail.com>
2026-08-29 16:46:21 +0100

    Delete class66_direct_rail_Serbices

--- catalogue.js
+++ catalogue.js
@@ -1871,30 +1871,6 @@ const CATALOGUE = [
     "pos": "50% 50%"
   },
   {
-    "id": "class66_direct_rail_Serbices",
-    "quizzes": {
-      "class": {
-        "name": "Class 66",
-        "blur": [],
-        "blurR": 12
-      },
-      "livery": {
-        "name": "Direct Rail Services",
-        "blur": [
-          [
-            323,
-            398,
-            348,
-            438
-          ]
-        ],
-        "blurR": 10
-      }
-    },
-    "zoom": 1.0,
-    "pos": "50% 50%"
-  },
-  {
     "id": "class_331_northern",
     "quizzes": {
       "class": {
